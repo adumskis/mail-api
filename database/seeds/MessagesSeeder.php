a719fdc06360a1b32de9512cf9e34b5f92135fcb
Algirdas Dumskis <algirdasdum@gmail.com>
2017-09-12 21:34:11 +0300

    Created seeder for messages

--- database/seeds/MessagesSeeder.php
+++ database/seeds/MessagesSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Message;
 
 class MessagesSeeder extends Seeder
 {
@@ -11,6 +12,17 @@ class MessagesSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $filePath = storage_path('seeds_data/messages_sample.json');
+        $content = file_get_contents($filePath);
+        // Second parameter set to true to get messages as array, no as stdClass
+        $data = json_decode($content, true);
+        if (isset($data['messages']) && is_array($data['messages'])) {
+            foreach ($data['messages'] as $messageData) {
+                $message = new Message($messageData);
+                // Message uid is not fillable
+                $message->uid = $messageData['uid'];
+                $message->save();
+            }
+        }
     }
 }
